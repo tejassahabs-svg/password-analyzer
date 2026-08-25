379dc63b606825aaefbd55c6b163361181d78c1c
tyz <tejassahabs@gmail.com>
2026-08-25 15:57:12 +0530

    updating password analyzer

---
+++ passwordAnalyzer.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class passwordAnalyzer
+{
+public static void main()
+{
+    Scanner sc = new Scanner(System.in);
+    System.out.println("HAVE DOUBTS ANALYZE YOUR PASSWORD HERE JUST DROP IT");
+     String p=sc.nextLine();
+     int l=p.length();
+}
+}
