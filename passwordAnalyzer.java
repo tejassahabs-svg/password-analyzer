52f032df037540d14d3e97cb7bf0681481055bdb
tyz <tejassahabs@gmail.com>
2026-08-25 17:34:23 +0530

    back to baseline

---
+++ passwordAnalyzer.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+class passwordAnalyzer {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("HAVE DOUBTS ANALYZE YOUR PASSWORD HERE JUST DROP IT");
+        String p = sc.nextLine();
+
+       
+}
